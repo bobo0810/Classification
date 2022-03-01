ba5c591309c97fbb52df1422b0177997c33ae093
lipengbo <lipengbo@kanzhun.com>
2022-03-01 11:51:37 +0800

    修改 自定义Backbone示例

--- Models/Backbone/mynet.py
+++ Models/Backbone/mynet.py
@@ -1,51 +1,36 @@
 import torch
 import torch.nn as nn
 from typing import Any
+import timm
 
 
-class AlexNet(nn.Module):
-    def __init__(self, num_classes: int = 1000) -> None:
-        super(AlexNet, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
+class Net(nn.Module):
+    """
+    示例：自定义backbone = timm特征提取层 + 自定义分类层
+    """
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def __init__(self, pretrained, num_classes):
+        super(Net, self).__init__()
+
+        # timm特征提取层
+        self.features = timm.create_model("efficientnet_b0", pretrained=pretrained)
+        self.features.reset_classifier(0)  # 丢弃timm模型的分类层
+
+        # 自定义分类层
+        self.add_linear = nn.Linear(1280, 512, bias=True)
+        self.act3 = nn.ReLU(inplace=True)
+        self.classifier = nn.Linear(512, num_classes, bias=True)
+
+    def forward(self, x):
         x = self.features(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
+        x = self.add_linear(x)
+        x = self.act3(x)
         x = self.classifier(x)
         return x
 
 
-def MyNet(pretrained=True, num_classes=1000):
+def MyNet(pretrained, num_classes):
     """
     自定义backbone
     """
-    model = AlexNet(num_classes=num_classes)
-    if pretrained:
-        # 加载与训练权重
-        pass
-    return model
+    return Net(pretrained, num_classes)
