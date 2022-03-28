db5cd7e00b6783159c63da2c322ce1d017aeded5
lipengbo <lipengbo@kanzhun.com>
2022-03-28 16:58:12 +0800

    删除权重加载旧版代码

--- Models/Backbone/__init__.py
+++ Models/Backbone/__init__.py
@@ -17,15 +17,11 @@ class create_backbone(nn.Module):
         """
         super(create_backbone, self).__init__()
         self.model = self.init_model(model_name, num_classes)
-        if checkpoint:
-            print("checkpoint load {}".format(checkpoint))
-            self.model = self.load_checkpoint(self.model, checkpoint)
 
     def forward(self, imgs):
         return self.model(imgs)
 
-    @staticmethod
-    def init_model(model_name, num_classes):
+    def init_model(self, model_name, num_classes):
         """
         初始化主干网络
         """
@@ -34,27 +30,3 @@ class create_backbone(nn.Module):
             pretrained=True,
             num_classes=num_classes,
         )
-
-    @staticmethod
-    def load_checkpoint(model, checkpoint):
-        """
-        加载预训练权重
-        """
-        state_dict = torch.load(checkpoint, map_location=torch.device("cpu"))
-        model_dict = model.state_dict()  # 模型参数
-        new_state_dict = OrderedDict()
-        # 遍历预训练参数
-        for k, v in state_dict.items():
-            name = k
-            # 移除前缀
-            if "module." in name:
-                name = name[7:]
-            if "model." in name:
-                name = name[6:]
-            if name in model_dict:
-                new_state_dict[name] = v
-            else:
-                print("!" * 6, "model parameter  mismatch:" + name, "!" * 6)
-        model_dict.update(new_state_dict)
-        model.load_state_dict(model_dict, strict=True)
-        return model
