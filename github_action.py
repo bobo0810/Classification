875f7763b741be190b9ce8f45c7a569821ea76d5
Mr.Li <im.lipengbo@foxmail.com>
2022-04-01 18:04:49 +0800

    fix

---
+++ github_action.py
@@ -0,0 +1,36 @@
+'''
+Github Action 实现训练、推理
+'''
+import os
+import argparse
+import glob
+cur_path = os.path.abspath(os.path.dirname(__file__))
+
+
+def train():
+    '''
+    训练
+    '''
+    os.system('python train.py')
+
+def predict():
+    '''
+    推理
+    '''
+    weights = glob.glob(cur_path+"/ExpLog/*/*/*.pt")[0]
+    print("load weights from ",weights)
+    os.system('python predict.py -weights %s  --vis_cam'%(weights))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Github Action")
+    parser.add_argument("--mode",type=str, default="train",help="train|predict")
+    args = parser.parse_args()
+
+    assert args.mode in ["train","predict"]
+    if args.mode == "train":
+        train()
+    elif args.mode == "predict":
+        predict()
+    else:
+        print("Invalid mode")
