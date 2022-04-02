57e25cab295af55cf2614d7536a1b9ccfbc21568
lipengbo <lipengbo@kanzhun.com>
2022-04-02 11:28:14 +0800

    githubaction支持部署

--- github_action.py
+++ github_action.py
@@ -18,21 +18,33 @@ def predict():
     '''
     推理
     '''
-    weights = glob.glob(cur_path+"/ExpLog/*/*/*.pt")[0]
+    weights = glob.glob(cur_path+"/ExpLog/*/*/*last.pt")[0]
     print("load weights from ",weights)
     os.system('python predict.py --weights %s  --vis_cam'%(weights))
     print("Predict Success!")
 
+def deploy():
+    '''
+    部署 torch->torchscript/onnx
+    '''
+    weights = glob.glob(cur_path+"/ExpLog/*/*/*last.pt")[0]
+    print("load weights from ",weights)
+    os.system('python export.py --weights %s  --torch2script  --torch2onnx '%(weights))
+    print("Deploy Success!")
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Github Action")
-    parser.add_argument("--mode",type=str, default="train",help="train|predict")
+    parser.add_argument("--mode",type=str, default="train",help="train|predict|deploy")
     args = parser.parse_args()
 
-    assert args.mode in ["train","predict"]
+    assert args.mode in ["train","predict","deploy"]
     if args.mode == "train":
         train()
     elif args.mode == "predict":
         predict()
+    elif args.mode == "deploy":
+        deploy()
     else:
         print("Invalid mode")
