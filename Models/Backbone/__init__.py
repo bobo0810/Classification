7c635675e0bf02fe08d1d83469d70f28aa412016
lipengbo <lipengbo@kanzhun.com>
2022-04-11 22:03:40 +0800

    简化模型加载

--- Models/Backbone/__init__.py
+++ Models/Backbone/__init__.py
@@ -6,28 +6,15 @@ from .mynet import mynet  # 注册自定义分类模型
 from .mynet_metric import mynet_metric  # 注册自定义特征提取模型
 
 
-class create_backbone(nn.Module):
+def create_backbone(model_name, num_classes):
     """
     主干网络入口
+
+    model_name: 模型名称,即yaml文件backbone属性值
+    num_classes: 类别数
     """
-
-    def __init__(self, model_name, num_classes):
-        """
-        model_name: 模型名称,即yaml文件backbone属性值
-        num_classes: 类别数
-        """
-        super(create_backbone, self).__init__()
-        self.model = self.init_model(model_name, num_classes)
-
-    def forward(self, imgs):
-        return self.model(imgs)
-
-    def init_model(self, model_name, num_classes):
-        """
-        初始化主干网络
-        """
-        return timm.create_model(
-            model_name,
-            pretrained=True,
-            num_classes=num_classes,
-        )
+    return timm.create_model(
+        model_name,
+        pretrained=True,
+        num_classes=num_classes,
+    )
