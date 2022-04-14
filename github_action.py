27c8e97186a195540df25f4acd61aac773208483
lipengbo <lipengbo@kanzhun.com>
2022-04-14 21:29:16 +0800

    fix

--- github_action.py
+++ github_action.py
@@ -1,45 +1,51 @@
-'''
+"""
 Github Action 实现训练、推理
-'''
+"""
 import os
 import argparse
 import glob
+
 cur_path = os.path.abspath(os.path.dirname(__file__))
 
 
 def train():
-    '''
+    """
     训练
-    '''
-    os.system('python train.py')
+    """
+    os.system("python train.py")
     print("Train Success!")
 
+
 def predict():
-    '''
+    """
     推理
-    '''
-    weights = glob.glob(cur_path+"/ExpLog/*/*/*last.pt")[0]
-    print("load weights from ",weights)
-    os.system('python predict.py --weights %s  --vis_cam'%(weights))
+    """
+    weights = glob.glob(cur_path + "/ExpLog/*/*/*.pt")[0]
+    print("load weights from ", weights)
+    os.system("python predict.py --weights %s  --vis_cam" % (weights))
     print("Predict Success!")
 
-def deploy():
-    '''
-    部署 torch->torchscript/onnx
-    '''
-    weights = glob.glob(cur_path+"/ExpLog/*/*/*last.pt")[0]
-    print("load weights from ",weights)
-    os.system('python export.py --weights %s  --torch2script  --torch2onnx '%(weights))
-    print("Deploy Success!")
 
+def deploy():
+    """
+    部署 torch->torchscript/onnx
+    """
+    weights = glob.glob(cur_path + "/ExpLog/*/*/*.pt")[0]
+    print("load weights from ", weights)
+    os.system(
+        "python export.py --weights %s  --torch2script  --torch2onnx " % (weights)
+    )
+    print("Deploy Success!")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Github Action")
-    parser.add_argument("--mode",type=str, default="train",help="train|predict|deploy")
+    parser.add_argument(
+        "--mode", type=str, default="train", help="train|predict|deploy"
+    )
     args = parser.parse_args()
 
-    assert args.mode in ["train","predict","deploy"]
+    assert args.mode in ["train", "predict", "deploy"]
     if args.mode == "train":
         train()
     elif args.mode == "predict":
