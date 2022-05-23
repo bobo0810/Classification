3a94cf600d8a6632d7d220b6d1cce653bf98ffaa
lipengbo <lipengbo@kanzhun.com>
2022-05-23 18:05:39 +0800

    新增人像比对预处理

--- DataSets/preprocess.py
+++ DataSets/preprocess.py
@@ -3,8 +3,13 @@ import torch
 import cv2
 import os
 import numpy as np
+import torchvision
+from torchvision.transforms import transforms
 from timm.data.transforms_factory import create_transform as timm_transform
-
+preprocess_list=[
+    ImageNet_PreProcess,# ImageNet预处理
+    Face_PreProcess,# 人脸比对预处理
+]
 
 def ImageNet_PreProcess(img_path, use_augment=False, img_size=[224, 224]):
     '''
@@ -31,4 +36,37 @@ def ImageNet_PreProcess(img_path, use_augment=False, img_size=[224, 224]):
         img_transforms = timm_transform(img_size)
     return img_transforms(img)
 
+def Face_PreProcess(img_path, use_augment=False, img_size=[112, 112]):
+    '''
+    人脸比对预处理
+    注：人脸比对数据集 默认已基于关键点裁剪并对齐。
 
+    img_path: 图像路径
+    use_augment:是否图像增广
+    img_size:训练图像尺寸
+    '''
+    assert os.path.exists(img_path), "图像文件不存在"
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    img = Image.fromarray(img)
+
+    if use_augment: 
+        # 增广 
+        img_transforms = torchvision.transforms.Compose(
+            [   
+                # TODO 噪声、模糊
+                torchvision.transforms.RandomHorizontalFlip(), # 镜像翻转
+                torchvision.transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1), # 色彩抖动
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(mean=(0.5,), std=(0.5,)),
+            ]
+        )
+
+    else: 
+        # 不增广
+        img_transforms = torchvision.transforms.Compose(
+            [   
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(mean=(0.5,), std=(0.5,)),
+            ]
+        )
+    return img_transforms(img)
