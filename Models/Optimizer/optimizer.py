950efb40b7d6efb27c412426cee127bc03c3e2ac
lipengbo <lipengbo@kanzhun.com>
2022-05-30 21:44:21 +0800

    fix

--- Models/Optimizer/optimizer.py
+++ Models/Optimizer/optimizer.py
@@ -1,22 +1,23 @@
 import timm
+import timm.optim
 
 
 def SGD(params, lr):
     """SGD优化器"""
-    timm.optim.create_optimizer_v2(
+    return timm.optim.create_optimizer_v2(
         params, opt="sgd", lr=lr, weight_decay=0.0005, momentum=0.9
     )
 
 
 def Adam(params, lr):
     """Adam优化器"""
-    timm.optim.create_optimizer_v2(
+    return timm.optim.create_optimizer_v2(
         params, opt="adam", lr=lr, weight_decay=0.0005, momentum=0.9
     )
 
 
 def Lamb(params, lr):
     """Lamb优化器"""
-    timm.optim.create_optimizer_v2(
+    return timm.optim.create_optimizer_v2(
         params, opt="lamb", lr=lr, weight_decay=0.0005, momentum=0.9
     )
