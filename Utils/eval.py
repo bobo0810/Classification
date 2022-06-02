6ec7599a6e2f68dfb4d27534ce5fdf4d9d163503
lipengbo <lipengbo@kanzhun.com>
2022-06-02 18:13:36 +0800

    度量学习支持常规格式评估

--- Utils/eval.py
+++ Utils/eval.py
@@ -1,5 +1,6 @@
 import torch
 from pycm import ConfusionMatrix
+from DataSets import create_datasets
 from pytorch_metric_learning import losses, testers
 from pytorch_metric_learning.utils.accuracy_calculator import AccuracyCalculator
 
@@ -38,24 +39,27 @@ def eval_metric_model(model, dataset, img_size, process_name, batch_size, mode):
     batch_size: 推理批次
     mode: 指定评估类型
     """
-    # assert mode in ["eval", "test"]
+    assert mode in ["val", "test"]
     if "positive_pairs" in dataset[mode].keys():
         # [类型,是否为同类,图片1,图片2] 样本对格式
         pass
     else:
-        pass
-        # [类型,类别名,图像路径]格式
-        # train_set
-        # val_set
+        # [类型,类别名,图像路径]格式   训练集的某类所有图片特征的均值作为该类的特征中心，测试样本与某类距离最近即判定为该类。
+        train_set = create_datasets(
+            dataset=dataset["train"], size=img_size, process=process_name
+        )
+        val_set = create_datasets(
+            dataset=dataset[mode], size=img_size, process=process_name
+        )
 
-        # tester = testers.BaseTester(batch_size=batch_size, dataloader_num_workers=4)
-        # train_embeddings, train_labels = tester.get_all_embeddings(train_set, model)
-        # test_embeddings, test_labels = tester.get_all_embeddings(val_set, model)
-        # train_labels, test_labels = train_labels.squeeze(1), test_labels.squeeze(1)
+        tester = testers.BaseTester(batch_size=batch_size, dataloader_num_workers=4)
+        train_embeddings, train_labels = tester.get_all_embeddings(train_set, model)
+        test_embeddings, test_labels = tester.get_all_embeddings(val_set, model)
+        train_labels, test_labels = train_labels.squeeze(1), test_labels.squeeze(1)
 
-        # accuracy_calculator = AccuracyCalculator(include=("precision_at_1",), k=1)
-        # accuracies = accuracy_calculator.get_accuracy(
-        #     test_embeddings, train_embeddings, test_labels, train_labels, False
-        # )
-        # precision = accuracies["precision_at_1"]
-    return 0.9
+        accuracy_calculator = AccuracyCalculator(include=("precision_at_1",), k=1)
+        accuracies = accuracy_calculator.get_accuracy(
+            test_embeddings, train_embeddings, test_labels, train_labels, False
+        )
+        precision = accuracies["precision_at_1"]
+        return precision
